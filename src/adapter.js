95e09bec5815b5b902d194a2b313055ead8f3e25
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-10-03 23:02:10 -0400

    created adapter

---
+++ src/adapter.js
@@ -0,0 +1,11 @@
+const baseURL = `https://www.flickr.com/services/api/`
+
+const Adapter = {
+
+  getAll: function getAll() {
+    return fetch(baseURL)
+    .then(resp => console.log())
+  }
+
+
+}
